947e4ed0679c8b81579125d27a6506c3cd7d2cef
alibekismagulov <na.ismagulov@gmail.com>
2023-08-21 23:57:46 +0400

    Initial Commit

---
+++ TestProject1221Systems/TestProject1221SystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProject1221SystemsApp.swift
+//  TestProject1221Systems
+//
+//  Created by Alibek Ismagulov on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestProject1221SystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
